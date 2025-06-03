a8c529a23e3e6b47197c85b9039226376aa193f3
GitGudBruhh <gitgudbruhh@tuta.io>
2025-06-03 23:42:01 +0530

    Added AI and SDS statement

--- src/components/Events/data.js
+++ src/components/Events/data.js
@@ -1,12 +1,28 @@
 const data = [
     {
-    heading: "PAGE UNDER CONSTRUCTION :)",
+    heading: "Fraudit - Bank Fraud Detection with Machine Learning",
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, why am i doing this :(",
-    image: "/Events/under-construction.jpg",
+      "Fraudit is all about using machine learning to catch shady transactions. You'll work with a dataset of bank activity and train simple ML models to figure out which transactions look suspicious. It’s like being a digital detective — spotting fraud using patterns in the data!!",
+    image: "/Events/ai-fraud.png",
     knowMoreLink:
-     "https://example.com",
+     "/Events/ai-fraud.pdf",
   },
+  {
+    heading: "Lunar Vision: Crater & Boulder Detection Challenge",
+    content:
+      "Develop an AI/ML model—preferably a CNN-based solution—to automatically detect and localize craters and boulders of various shapes and sizes from high-resolution lunar surface imagery. The model should output bounding boxes around the detected features.",
+    image: "/Events/sds-lunar.jpg",
+    knowMoreLink:
+     "/Events/sds-lunar.pdf",
+  },
+  // {
+  //   heading: "Mission Mars: Emergency Reaction Wheel Redesign",
+  //   content:
+  //     "You are part of a Mars-bound mission aboard the spacecraft Aether-1. Midway through the journey, the spacecraft begins to tumble — your primary reaction wheel has failed.  Your task is to design, power, and control a brand-new reaction wheel system that can stabilize the spacecraft before it drifts off-course permanently.",
+  //   image: "/Events/sds-mars.jpeg",
+  //   knowMoreLink:
+  //    "/Events/sds-mars.pdf",
+  // },
 ];
 
 export default data;
--- src/components/Timeline/timeline_data.js
+++ src/components/Timeline/timeline_data.js
@@ -1,12 +1,28 @@
 let timeLineData = [
   {
-    title: "Good Things are worth the wait",
-    location: "Talaab / Swimming Pool @ IIT-Dh (\"under construction\")",
-    description: " Even I dont know what it is",
-    date: "Bro fr . . . ?",
-    color: "#ffffff",
-    icon: "Coding",
-  }
+    title: "Fraudit - Bank Fraud Detection",
+    location: "Hardly Human - AI Club",
+    description: "Use machine learning to analyze bank transactions and identify suspicious activities like a digital detective.",
+    date: "3rd June 2025",
+    color: "#00ffff",
+    icon: "AI",
+  },
+  {
+    title: "Lunar Vision: Crater & Boulder Detection",
+    location: "Cosmosoc - Space Data Science Club",
+    description: "Build a CNN model to effectively detect and highlight craters and boulders in lunar imagery with bounding boxes.",
+    date: "3rd June 2025",
+    color: "#f6d32d",
+    icon: "SDS",
+  },
+  // {
+  //   title: "Mission Mars: Emergency Reaction Wheel Redesign",
+  //   location: "Cosmosoc - Space Data Science Club",
+  //   description: "Design, power, and control a new reaction wheel system to stabilize the tumbling Aether-1 spacecraft on its Mars-bound mission before it drifts off-course.",
+  //   date: "3rd June 2025",
+  //   color: "#ff0000",
+  //   icon: "SDS",
+  // }
 ]
 
 export default timeLineData
